ad18e71ddebe7284f01d20a0e5d583d4891650b6
Shobhit Gupta <shobhit.gupta.compscience@gmail.com>
2017-04-26 17:58:08 +0530

    feat: Prep the collection view cell to display an overlay when selected

--- MemeMe/View/MemeCollectionViewCell.swift
+++ MemeMe/View/MemeCollectionViewCell.swift
@@ -8,7 +8,47 @@
 
 import UIKit
 
+
 class MemeCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var overlay: OverlayView!
+    
+    override var isSelected: Bool {
+        didSet {
+            overlay.isHidden = !isSelected
+            imageView.layer.borderWidth = isSelected ? 2 : 0
+            
+        }
+    }
+    
+    
+    // MARK: Initializers
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+        imageView.layer.borderColor = ArtKit.highlightOfPrimaryColor.cgColor
+        isSelected = false
+    }
+    
+}
+
+
+//******************************************************************************
+//                              MARK: Setup
+//******************************************************************************
+extension MemeCollectionViewCell {
+    
+    fileprivate func setupView() {
+        setupOverlay()
+    }
+    
+    
+    private func setupOverlay() {
+//        let properties = OverlayType.Properties(color: UIColor.white.withAlphaComponent(0.2), blur: nil)
+        let properties = OverlayType.Properties(color: UIColor.white.withAlphaComponent(0.2), blur: OverlayType.Properties.Blur(style: .light, isVibrant: false))
+        _ = overlay.setupOverlay(withDesired: properties)
+        overlay.alpha = 0.5
+    }
+    
 }
